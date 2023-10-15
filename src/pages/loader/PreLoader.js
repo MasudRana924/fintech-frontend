30800aca68ee64a2098d39a6c9b8d0bf3caea672
Masud Rana <masud15-924@diu.edu.bd>
2023-10-15 21:18:51 +0600

    preloader added

--- src/pages/loader/PreLoader.js
+++ src/pages/loader/PreLoader.js
@@ -1,29 +1,17 @@
 import React from 'react';
-import logo from '../../images/logo (2).png';
 import './Loader.css'
-import { ThreeDots } from 'react-loader-spinner';
 const PreLoader = () => {
     return (
         <div className="w-full  mx-auto md:w-2/4 lg:w-1/4 lg:mx-auto bg-violet-500">
         <div className="ml-5 w-3/4 mx-auto lg:ml-28  container">
             <div className="flex flex-1 items-center justify-center ">
                 <div >
-                <p className="pt-72 pl-12 text-3xl text-white pre-title-text">ট্রাস্ট-পে</p>
-                <div className="pl-20">
-                <ThreeDots 
-                     color="#fff"
-                    strokeWidth="3"
-                    animationDuration="0.75"
-                    width="48"
-                    visible={true}
-                />
+                <p className="pt-72 pl-12 text-4xl text-white pre-title-text">ট্রাস্ট-পে</p>
                 </div>
-                </div>
-
             </div>
         </div>
         <div className="w-1/4 mx-auto">
-        <p className=" text-center text-white text-sm version-text">version 1.0</p>
+        <p className=" text-center text-white text-sm version-text ">version 1.0</p>
         </div>
     </div>
     );
