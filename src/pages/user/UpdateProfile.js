9f310b9981af4987ec8e5646baf723f4be8154dc
MasudRana15924 <masudrana15924@outlook.com>
2023-11-07 23:18:50 +0600

    UI updated

--- src/pages/user/UpdateProfile.js
+++ src/pages/user/UpdateProfile.js
@@ -5,16 +5,15 @@ import { Link, useNavigate } from 'react-router-dom';
 import './Profile.css'
 import { useDispatch, useSelector } from 'react-redux';
 import { cleanAvatarSuccess, updateAvatar } from '../../state/user/updateavatarSlice';
-import { InputAdornment, TextField } from '@mui/material';
 import { message } from 'antd';
 
 const UpdateProfile = () => {
     const dispatch = useDispatch();
-    const navigate=useNavigate()
+    const navigate = useNavigate()
     const { user } = useSelector(
         (state) => state.userdetails.userdetails
     );
-    const { updateAvatarSuccess,isLoading } = useSelector(
+    const { updateAvatarSuccess, isLoading } = useSelector(
         (state) => state.updatedAvatar
     );
     const { loggeduser } = useSelector(
@@ -23,8 +22,6 @@ const UpdateProfile = () => {
     const userToken = loggeduser.token;
     const [avatar, setAvatar] = useState();
     const [avatarPreview, setAvatarPreview] = useState();
-    // const [firstname, setFirstName] = useState('');
-    // const [lastname, setLastName] = useState('');
     const updateProfileDataChange = (e) => {
         const reader = new FileReader();
         reader.onload = () => {
@@ -38,25 +35,21 @@ const UpdateProfile = () => {
     useEffect(() => {
         if (user) {
             setAvatarPreview(user?.avatarLogo);
-            // setFirstName(user.firstname)
-            // setLastName(user.lastname)
         }
-        if(updateAvatarSuccess){
+        if (updateAvatarSuccess) {
             message.success("আপডেট সম্পুর্ণ হয়েছে");
             navigate('/main')
             dispatch(cleanAvatarSuccess());
         }
-    }, [user,updateAvatarSuccess,dispatch,navigate]);
+    }, [user, updateAvatarSuccess, dispatch, navigate]);
     const updateProfileSubmit = (e) => {
         e.preventDefault();
         const formData = new FormData();
-        // formData.append('firstname', firstname);
-        // formData.append('lastname', lastname);
         formData.append('avatarLogo', avatar);
         dispatch(updateAvatar({ userToken, data: formData }));
     };
     return (
-        <div className="w-fulllg:w-1/4 lg:mx-auto  lg:border lg:rounded-lg lg:shadow-lg ">
+        <div className="w-fulllg:w-1/4 lg:mx-auto  lg:border lg:rounded-lg lg:shadow-lg  ">
             <div className="flex bg-violet-500 h-16 rounded-b-lg ">
                 <div className="w-1/4">
                     <Link to="/main">
@@ -65,17 +58,13 @@ const UpdateProfile = () => {
                 </div>
                 <p className="text-white text-md mt-4 ml-5">প্রোফাইল আপডেট</p>
             </div>
-
             <form action="" className="space-y-6 mt-12  " onSubmit={updateProfileSubmit}>
                 <div className="image-section  flex flex-1 items-center justify-center gap-2 mt-16">
-
                     <img
                         alt=""
                         src={avatarPreview}
-                        // className="h-36 w-36 border-2 border-violet-500 rounded-full  avatar-image"
-                        className={isLoading ? "h-36 w-36 border-2 border-violet-500 rounded-full  avatar-image":"h-36 w-36 border-2 border-violet-500 rounded-full"}
+                        className={isLoading ? "h-36 w-36 border-2 border-violet-500 rounded-full  avatar-image" : "h-36 w-36 border-2 border-violet-500 rounded-full"}
                     />
-
                     <label className='border border-violet-500 bg-violet-500'>
                         +
                         <br />
@@ -88,37 +77,7 @@ const UpdateProfile = () => {
                         />
                     </label>
                 </div>
-                {/* <TextField
-                    id="input-with-icon-textfield"
-                    // label="Phone"
-                    label="নামের প্রথম অংশ"
-                    InputProps={{
-                        startAdornment: (
-                            <InputAdornment position="start">
-
-                            </InputAdornment>
-                        ),
-                    }}
-                    variant="outlined"
-                    className="w-full m-4"
-                    value={firstname} onChange={(e) => setFirstName(e.target.value)}
-                />
-                <TextField
-                    id="input-with-icon-textfield"
-                    // label="Password"
-                    label="নামের শেষ অংশ"
-                    InputProps={{
-                        startAdornment: (
-                            <InputAdornment position="start">
-
-                            </InputAdornment>
-                        ),
-                    }}
-                    variant="outlined"
-                    className="w-full py-3 px-6"
-                    value={lastname} onChange={(e) => setLastName(e.target.value)}
-                /> */}
-                <div className="success-btn w-full">
+                <div className="submit-bottom-btn w-full">
 
                     <button className="h-12 w-full  bg-violet-500 border-violet-500 hover:bg-violet-500 hover:border-violet-500 mb-5">
                         <span className="font-semibold text-white text-lg">পরবর্তী</span>
@@ -126,7 +85,6 @@ const UpdateProfile = () => {
 
                 </div>
             </form>
-
         </div>
     );
 };
