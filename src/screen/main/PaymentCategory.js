0ae22cf49b518d1af2fb7f1cb36de2c336c291eb
MasudRana15924 <masudrana15924@outlook.com>
2023-12-23 13:59:39 +0600

    UI updated

--- src/screen/main/PaymentCategory.js
+++ src/screen/main/PaymentCategory.js
@@ -10,22 +10,21 @@ import './Main.css'
 
 
 const PaymentCategory = () => {
-
     return (
         <div className="mt-16 lg:mt-24 border border-white bg-white rounded-lg ml-2 mr-2 payment-category-section">
             {/* <p className="text-start text-xs m-4">পি-ক্যাশ সেবাসমূহ</p> */}
             <div className="flex justify-around mt-4 mb-4">
-                <div className="border rounded-lg p-2 w-20 h-16">
+                <div className=" bg-teal-300 border rounded-lg p-2 w-20 h-16">
                     <Link to="/send">
 
                         <div className="flex justify-center">
-                            <MdOutlineSendToMobile className="text-3xl  text-amber-500  mb-1">ট</MdOutlineSendToMobile>
-                            <p className="text-sm font-thin text-amber-500 mb-1 mt-2">ট</p>
+                            <MdOutlineSendToMobile className="text-3xl  text-red-500  mb-1">ট</MdOutlineSendToMobile>
+                            <p className="text-sm font-thin text-red-500 mb-1 mt-2">ট</p>
                         </div>
-                        <p className="paymentCategory-text font-medium text-center">সেন্ড মানি</p>
+                        <p className="paymentCategory-text  font-medium text-center">সেন্ড মানি</p>
                     </Link>
                 </div>
-                <div className="border rounded-lg p-2 w-20 h-16">
+                <div className="bg-fuchsia-300 border-fuchsia-300 rounded-lg p-2 w-20 h-16">
                     <Link to="/cashout">
                         <div className="flex justify-center">
                             <BiMenuAltLeft className="text-3xl  text-lime-500  mb-1">ট</BiMenuAltLeft>
@@ -34,7 +33,7 @@ const PaymentCategory = () => {
                         <p className="paymentCategory-text  font-medium  text-start" >ক্যাশ আউট</p>
                     </Link>
                 </div>
-                <div className="border rounded-lg p-2 w-20 h-16">
+                <div className="bg-cyan-300 border-cyan-300 rounded-lg p-2 w-20 h-16">
                     <Link to="/recharge">
                         <div className="flex justify-center">
                             <BsLightningCharge className="text-3xl text-violet-500  mb-1 "></BsLightningCharge>
@@ -42,7 +41,7 @@ const PaymentCategory = () => {
                         <p className="paymentCategory-text font-medium  text-center">রিচার্জ</p>
                     </Link>
                 </div>
-                <div className="border rounded-lg p-2 w-20 h-16">
+                <div className="border rounded-lg p-2 w-20 h-16 bg-amber-300 border-amber-300">
                     <Link to="/payment/number">
                         <div className="flex justify-center">
                             <BsBagFill className="text-3xl text-blue-400 mb-1 "></BsBagFill>
@@ -53,7 +52,7 @@ const PaymentCategory = () => {
                 </div>
             </div>
             <div className="mt-5 flex justify-around mb-4" >
-                <div className="border rounded-lg p-2 w-20 h-16">
+                <div className="bg-blue-300 border-blue-300 rounded-lg p-2 w-20 h-16">
                     <Link to="/add/money">
                         <div className="flex justify-center">
                             <AiOutlineSend className="text-3xl text-teal-500 mb-1"></AiOutlineSend>
@@ -61,14 +60,14 @@ const PaymentCategory = () => {
                         <p className="paymentCategory-text text-center font-medium ">এ্যাড মানি</p>
                     </Link>
                 </div>
-                <div className="border rounded-lg p-2 w-20 h-16">
+                <div className="bg-violet-300 border-violet-300 rounded-lg p-2 w-20 h-16">
                     <Link to="/pay/bill">
                         <div className="flex justify-center">
                             <BsLightbulb className="text-3xl  text-red-500  mb-1"></BsLightbulb>
                         </div>
                         <p className="paymentCategory-text text-center  font-medium">পে-বিল</p></Link>
                 </div>
-                <div className="border rounded-lg p-2 w-20 h-16">
+                <div className="bg-rose-300 border-rose-300 rounded-lg p-2 w-20 h-16">
                     <Link to="/savings">
                         <div className="flex justify-center">
                             <GiChipsBag className="text-3xl text-teal-700  mb-1"></GiChipsBag>
@@ -76,7 +75,7 @@ const PaymentCategory = () => {
                         <p className="paymentCategory-text text-center font-medium">সেভিংস</p>
                     </Link>
                 </div>
-                <div className="border rounded-lg p-2 w-20 h-16">
+                <div className="bg-emerald-300 border-emerald-300 rounded-lg p-2 w-20 h-16">
                     <Link to="/apply/loan">
                         <div className="flex justify-center">
                             <FaHandHoldingUsd className="text-3xl text-fuchsia-500 mb-1"></FaHandHoldingUsd>
@@ -86,7 +85,7 @@ const PaymentCategory = () => {
                 </div>
             </div>
             <div className="mt-5 ml-2 mb-4" >
-                <div className="border rounded-lg p-2 w-20 h-16">
+                <div className=" bg-orange-300 border-orange-300 rounded-lg p-2 w-20 h-16">
                     <Link to="/education/bill">
                         <div className="flex justify-center">
                             <BsBook className="text-3xl   text-cyan-400  mb-1"></BsBook>
